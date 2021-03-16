fe4cbbf88fa13fb206bb083cb2917ede74d7be9b
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2021-03-16 22:31:12 +0530

    Update private.tf

--- code/private.tf
+++ code/private.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "db-1" {
   ami                    = lookup(var.amis, var.aws_region)
   availability_zone      = "us-east-1a"
   instance_type          = "m1.small"
-  key_name               = var.aws_key_name
+  #key_name               = var.aws_key_name
   vpc_security_group_ids = ["${aws_security_group.db.id}"]
   subnet_id              = aws_subnet.us-east-1a-private.id
   source_dest_check      = false
