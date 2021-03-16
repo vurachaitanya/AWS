e29ddc096220cf23af7c8a107444b1dac2c8b265
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2021-03-16 22:30:39 +0530

    Update public.tf

--- code/public.tf
+++ code/public.tf
@@ -46,9 +46,9 @@ resource "aws_security_group" "web" {
 
 resource "aws_instance" "web-1" {
   ami                         = lookup(var.amis, var.aws_region)
-  availability_zone           = "eu-west-1a"
+  availability_zone           = "us-east-1a"
   instance_type               = "m1.small"
-  key_name                    = var.aws_key_name
+  #key_name                    = var.aws_key_name
   vpc_security_group_ids      = ["${aws_security_group.web.id}"]
   subnet_id                   = aws_subnet.us-east-1a-public.id
   associate_public_ip_address = true
